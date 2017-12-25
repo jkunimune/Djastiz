3d5ec85a3b2e5da6527749ea462ce03d3d153a02
Justin Kunimune <justinkunimune@gmail.com>
2017-12-24 15:48:06 -1000

    I'm trying this new idea called "verbs"
    The base of any language is its vocabulary. I'm rewriting this one to have more letters, shorter words, and different parts of speech. Chief among these being verbs. There are now 28 letters.

--- word_generator.py
+++ word_generator.py
@@ -4,87 +4,95 @@ import os
 import random
 
 
-vowels = [['a','e','o'], ['i','u']]
-consonants = ['k','g','t','d','p','b','s','z','c','j']
+CONSONANTS = [{'k','t','p'},{'g','d','b'}, {'h','th','f'},{'y','dh','v'}, {'ng','n','m'}, {'sh','ss','s'},{'zh','j','z'}, {'l'}]
+VOWELS = [{'e','a','o'}, {'i','r','u'}]
 
-char_type = {'':-1,'a':0,'e':0,'o':0,'i':0,'u':0,'k':1,'t':1,'p':1,'g':1,'d':1,'b':1,'s':2,'c':2,'z':2,'j':2,}
+VALID_CHAINS = [[5,7], [6,7], [7], [4,7], [], [0,4,7], [4,7], []]
+
+
+def random_choice(s):
+	"""because random_choice doesn't work for sets"""
+	return random.choice(tuple(s))
 
 
 def similar(a, b):
 	"""return True if a and b might sound too similar"""
-	for i in range(3):
-		if len(a) > len(b):
-			a, b = b, a
-		if len(a) < len(b):
-			return a == b[1:] or a == b[:-1]
-		else:
-			one_diff = False
-			for i in range(len(a)):
-				if char_type[a[i]] != char_type[b[i]]:
-					return False
-				if a[i] != b[i]:
-					if one_diff:
-						return False
-					one_diff = True
-			return True
+	return a == b
+	# for i in range(3):
+	# 	if len(a) != len(b):
+	# 		return False
+	# 	else:
+	# 		one_diff = False
+	# 		for i in range(len(a)):
+	# 			if char_type[a[i]] != char_type[b[i]]:
+	# 				return False
+	# 			if a[i] != b[i]:
+	# 				if one_diff:
+	# 					return False
+	# 				one_diff = True
+	# 		return True
 
 
-def random_vowel(nxt):
-	"""pick a vowel"""
-	options = copy.copy(vowels[0])+copy.copy(vowels[1])
-	if nxt != '' and nxt in options:
-		options.remove(nxt)
-	return random.choice(options)
+def length(word):
+	"""the number of Djastiz letters in the word"""
+	return len(word.replace('th',' ').replace('dh',' ').replace('sh',' ').replace('zh',' ').replace('ng',' ').replace('ss',' '))
 
 
-def random_consonant():
-	"""pick 1-2 pronounceable random consonants"""
-	return random.choice(consonants)
-
-
-def strong_vowel(nxt):
-	"""pick one of the strong vowels that is not equal to next"""
-	l = copy.copy(vowels[1])
-	if nxt in l:
-		l.remove(nxt)
-	return random.choice(l)
-
-
-def new_vowel_group(next_letter=''):
-	"""generate 1-2 vowels that make a single sound together"""
-	if char_type[next_letter] != 0 and random.random() < .15:
-		return random.choice(vowels[0]) + random.choice(vowels[1])
+def new_consonant_group(l_allowed=0):
+	""" generate a string of CONSONANTS that make a single sound together
+		l_allowed is 0 if 'l' is not allowed at all, 1 if 'l' is always allowed, and -1 if 'l' is allowed, but only if it's alone"""
+	if l_allowed != 0:
+		row = random.randrange(0,len(CONSONANTS))
 	else:
-		return random_vowel(next_letter)
+		row = random.randrange(0,len(CONSONANTS)-1)
+	group = random_choice(CONSONANTS[row])
+	if VALID_CHAINS[row] and random.random() < .4:
+		row = random_choice(VALID_CHAINS[row])
+		if row != 7 or (l_allowed == 1 and group not in ('t','d','h','y')):
+			group += random_choice(CONSONANTS[row])
+	return group
 
 
-def new_consonant_group(next_letter=''):
-	"""generate a consonant and/or a strong vowel"""
-	if next_letter == '' or random.random() < .9:
-		return random_consonant()
-	elif random.random() < .6:
-		return strong_vowel(next_letter[0])
+def new_strong_vowel(previous=''):
+	"""choose a consonant-like vowel that fits here"""
+	if previous.endswith('sh'):
+		return 'i'
+	elif previous.endswith('ss'):
+		return 'r'
+	elif previous.endswith('s') or previous.endswith('d') or previous.endswith('t'):
+		return 'u'
 	else:
-		return random_consonant()+strong_vowel(next_letter[0])
+		return random_choice(VOWELS[1])
+
+
+def new_vowel_group(previous=''):
+	"""generate 1-2 VOWELS that make a single sound together"""
+	last_letter = {previous[-1:]}
+	if random.random() < .2:
+		return random_choice(VOWELS[0]) + random_choice(VOWELS[1]-last_letter)
+	else:
+		return random_choice(VOWELS[0]|VOWELS[1]-last_letter)
 
 
 def new_word(num_syl):
 	"""generate a random word with num_syl syllables"""
 	if random.random() < .7:
-		word = new_consonant_group()
+		word = new_consonant_group(l_allowed=0)
 	else:
 		word = ''
-	word = new_vowel_group() + word
+	if random.random() < .3:
+		word += new_strong_vowel(word)
+	word += new_vowel_group(word)
+
 	for i in range(num_syl-1):
-		c = new_consonant_group(word[0])
-		v = new_vowel_group(c[0])
-		word = v+c+word
+		if random.random() < .8:
+			word += new_consonant_group(l_allowed=1)
+		else:
+			word += new_strong_vowel()
+		word += new_vowel_group(word)
+
 	if random.random() < .7:
-		word = new_consonant_group(word[0])+word
-	
-	for i in range(len(word)-1, 0, -1): # remove duplicate letters
-		if word[i] == word[i-1]:
-			word = word[0:i]+word[i+1:len(word)]
+		word += new_consonant_group(l_allowed=-1)
 	return word
 
 
@@ -92,17 +100,20 @@ def load_all_words(directory, max_size=0):
 	"""return a list of all the words in this directory"""
 	words = []
 	for filename in os.listdir(directory):
-		with open(directory+'\\'+filename, 'r') as f:
-			lines = f.readlines()
-			if max_size == 0 or len(lines) < max_size:
-				for line in lines:
-					if len(line) > 1:
-						try:
-							words.append(line[line.index(',')+1:].replace('sh','c').replace('\n',''))
-						except ValueError:
-							words.append(line.replace('sh','c').replace('\n',''))
-						if words[-1] == '':
-							print("! {}".format(filename))
+		if 'cache' not in filename:
+			with open(directory+'\\'+filename, 'r') as f:
+				lines = f.readlines()
+				if max_size == 0 or len(lines) < max_size:
+					for line in lines:
+						if len(line) > 1:
+							try:
+								djastiz_word = line[line.rindex(',')+1:].strip()
+							except ValueError:
+								djastiz_word = line.strip()
+							if djastiz_word:
+								words.append(djastiz_word)
+							# if words[-1] == '':
+							# 	print("! {}".format(filename))
 	return words
 
 
@@ -124,20 +135,18 @@ def check_words(directory):
 
 def generate_dictionary(num_words_syl=[5,5], seed=None, filename=None):
 	"""generate a bunch of random unique words"""
-	if seed == None: #pre-existing words
+	if seed is None:
 		seed = []
 
-	cache = [] #words from previous syllable counts
-
+	words = []
 	for i, num_words in enumerate(num_words_syl): # for each syllable-count set
-		words = []
 		num_syl = i+1
 		j = 0
 		while j < num_words: # until you have created the correct number of words
 			word = new_word(num_syl) # think of a word
 
 			too_similar = False # check that it is not too similar to any other words
-			for w in seed+cache+words:
+			for w in seed+words:
 				if similar(word, w):
 					too_similar = True
 					break
@@ -146,9 +155,9 @@ def generate_dictionary(num_words_syl=[5,5], seed=None, filename=None):
 			min_idx, max_idx = 0, len(words)
 			while True:
 				idx = (min_idx+max_idx)//2
-				if idx >= 0 and idx < len(words) and len(words[idx])//num_syl <= len(word)//num_syl:
+				if idx >= 0 and idx < len(words) and (length(words[idx])-1)//2 <= (length(word)-1)//2:
 					min_idx = idx+1
-				elif idx > 0 and idx <= len(words) and len(words[idx-1])//num_syl > len(word)//num_syl:
+				elif idx > 0 and idx <= len(words) and (length(words[idx-1])-1)//2 > (length(word)-1)//2:
 					max_idx = idx-1
 				else:
 					words.insert(idx, word) # insert it into the list, sorted by number of characters
@@ -157,16 +166,13 @@ def generate_dictionary(num_words_syl=[5,5], seed=None, filename=None):
 
 			if j % 1000 == 0:
 				with open(filename+'.csv','w') as f:
-					for word in cache:
-						f.write(word+'\n')
 					for word in words:
 						f.write(word+'\n')
-		cache = cache+words
-	return cache
+	return words
 
 
 if __name__ == '__main__':
-	print(check_words('dictionary'))
-	# words = generate_dictionary([0,20], seed=load_all_words('dictionary'))
-	# for word in words:
-	# 	print(word)
+	# print(check_words('dictionary'))
+	words = generate_dictionary([10000, 100000], seed=load_all_words('dictionary'), filename='dictionary/word_cache')
+	for word in words:
+		print(word)
