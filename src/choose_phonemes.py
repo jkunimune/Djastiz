7e22b0111e0cfe88720d4638bf4674db0ae484c4
Justin Kunimune <justinkunimune@gmail.com>
2018-05-04 10:17:58 -0300

    Welcome home, /f/.
    I reviewed my phonetic inventory again and found that I can, indeed, include /f/. Actually, I can have /r/, too, with my original criteria, but I found that it disadvantaged too many people too far, so I've decided that I want to keep the number of native pronouncers above 50% _and_ the number of average phonemes to learn below 1.
    I also had another insight stroke: onomatopoeias. I set the words for anything with a distinct, recognisable sound to it to be represented by its onomatopoeia, since that way _everyone_ will recognise the word more or less.
    I also added religions to the list of loanwords I want to have while I was at it.

--- src/choose_phonemes.py
+++ src/choose_phonemes.py
@@ -7,19 +7,21 @@ import csv
 import matplotlib.pyplot as plt
 import pickle
 
-MODIFIERS = {'ː', 'ˑ', 'ʷ', 'ʲ', 'ˠ', 'ˤ', 'ˀ'}
+MODIFIERS = {'ː', 'ˑ', 'ʷ', 'ʲ', 'ˠ', 'ˤ', 'ˀ'} #accents that are more important on their own than in combo with other sounds
+ACCENTS = {'̞', '̟', '̠', '̪', '͉', '̻'} #accents that my data source abuses and that mean nothing to me
 
-COMBOS = {('a','a̟','ɑ','ɐ'), ('e','e̞','ɛ'), ('o','o̞','ɔ'),
+COMBOS = {('a','ɑ','ɐ'), ('e','ɛ'), ('o','ɔ'),
 		('i','ɪ'), ('u','ʊ','ɯ'), ('j','i'), ('w','u'),
-		('ʃ','s̪','s','ʂ'), ('ʒ','z̪','z','ʐ'), ('ts','t̪s̪','t̠ʃ','ʈʂ'), ('dz','d̪z̪','d̠ʒ'),
-		('s̪','s','ts','t̪s̪'), ('z̪','z','dz','d̪z̪'), ('t̠ʃ','ʃ','ʈʂ','ʂ'), ('d̠ʒ','ʒ','ʐ'),
-		('s','s̪'), ('z','z̪'), ('n','n̪'), ('m','ɱ'),
-		('r','ɾ','ɽ','ɻ','ʁ','ʀ','r̪','ɹ'), ('l','ɾ','r'),
-		('h','ħ','χ','x', 'ɦ'), ('ɦ','ʕ','ʁ','ɣ'), ('f','ɸ'), ('v','β'),
-		('g','gʰ','kʰ'), ('d','d̪','dʰ','d̪ʰ','tʰ','t̪ʰ'), ('b','bʰ','pʰ'),
-		('k','g','ʰ'), ('t','t̪','d','d̪','ʰ'), ('p','b','ʰ')}
+		('ʃ','s','ʂ'), ('ʒ','z','ʐ'), ('ts','tʃ','ʈʂ'), ('dz','dʒ'),
+		('s','ts'), ('z','dz'), ('tʃ','ʃ','ʈʂ','ʂ'), ('dʒ','ʒ','ʐ'),
+		('r','ɾ','ɽ','ɻ','ʁ','ʀ','ɹ'), ('l','ɾ'),
+		('h','ħ','χ','x', 'ɦ'), ('ʕ','ʁ','ɣ'), ('f','ɸ'), ('v','β'),
+		('kʰ','k'), ('tʰ','t'), ('pʰ','p'),
+		('k','ɡ','ʰ'), ('t','d','ʰ'), ('p','b','ʰ')}
 
-DJASTIZ = {'/e/', '/a/', '/o/', '/i/', '/u/', '/j/', '/l/', '/w/', '/n/', '[m]', '/h/', '[s]', '/t̠ʃ/', '/k/', '/t/', '/p/'}
+# CASTISU = {'/pʰ/', '[b]', '/tʰ/', '[d]', '/kʰ/', '[ɡ]', '/tʃ/', '/dʒ/', '/f/', '/s/', '/z/', '/h/',
+# 			'/r/', '[m]', '[n]', '[ŋ]', '/l/', '/j/', '/w/', '/i/', '/u/', '/e/', '/o/', '/a/'}
+CASTISU = {'/p/', '/t/', '/k/', '/tʃ/', '/f/', '/s/', '/h/', '[m]', '[n]', '/w/', '/l/', '/j/', '/u/', '/i/', '/o/', '/a/', '/e/'}
 
 
 with open('..\\data\\ethnologue.pkl', 'rb') as f:
@@ -33,6 +35,7 @@ with open('..\\data\\phoible-phonemes.tsv', 'r', encoding='utf-8') as f:
 	language_names = {}
 	for row in reader:
 		data_idx, lang_code, lang_name, phoneme, clazz = row[0], row[2], row[3], row[7], row[8]
+
 		if header is None:
 			header = row
 			continue
@@ -45,6 +48,8 @@ with open('..\\data\\phoible-phonemes.tsv', 'r', encoding='utf-8') as f:
 				if mod in phoneme:
 					phoneme = phoneme.replace(mod,'')
 					inventories[lang_code].add('{}[{}]:'.format(clazz[0].upper(), mod))
+			for accent in ACCENTS: #pull out some useless accents, but don't make note of them
+				phoneme = phoneme.replace(accent, '') #go away!
 			if clazz == 'tone': #with tones, the number matters more than the exact value
 				for num_tones in range(10,-1,-1):
 					if str(num_tones)+'T: ' in inventories[lang_code] or num_tones <= 0: #count tones
@@ -58,6 +63,13 @@ with open('..\\data\\phoible-phonemes.tsv', 'r', encoding='utf-8') as f:
 				elif 'ʰ' in allophone_set and 'ʰ' in phoneme and phoneme.replace('ʰ','') in allophone_set:
 					inventories[lang_code].add('/'+allophone_set[0]+'/:')
 
+for lang_code, inventory in inventories.items(): #get useful information about stop distinctions
+	if '[k]:' in inventory and '[ɡ]:' in inventory: 	inventory.add('[VP]:') #voiced/voiceless plosive distinction
+	if '[k]:' in inventory and '[kʰ]:' in inventory: 	inventory.add('[AP]:') #aspirated/unaspirated plosive distinction
+	if '[ɡ]:' in inventory and '[kʰ]:' in inventory: 	inventory.add('[XP]:') #voiceless aspirated/voiced unasp
+	if '[VP]:' in inventory or '[XP]:' in inventory:  	inventory.add('/VP/:')
+	if '[AP]:' in inventory or '[XP]:' in inventory:  	inventory.add('/AP/:')
+
 phonemes = {}
 total_pop = 0
 for lang_code, inventory in inventories.items():
@@ -66,13 +78,13 @@ for lang_code, inventory in inventories.items():
 		phonemes[phoneme] = phonemes.get(phoneme,0) + population
 	total_pop += population
 
-num_who_must_learn = [0]*(len(DJASTIZ)+1) #find the number of humans who must learn _x_ new phonemes to speak Djastiz
+num_who_must_learn = [0]*(len(CASTISU)+1) #find the number of humans who must learn _x_ new phonemes to speak Djastiz
 avg_num_to_learn = 0
 specific_needs = {}
 for lang_code, inventory in inventories.items():
 	population = POPULATIONS.get(lang_code, (0,0))[1]
 	num_to_learn = 0
-	for phoneme in DJASTIZ:
+	for phoneme in CASTISU:
 		if phoneme+':' not in inventory:
 			num_to_learn += 1
 			if population:
@@ -87,7 +99,7 @@ for i, num in enumerate(num_who_must_learn):
 		print('{:.2f}% of humans can pronounce Djastiz words natively.'.format(num/total_pop*100))
 	else:
 		print('{:.2f}% of humans must learn {} new phonem{} to pronounce Djastiz words.'.format(num/total_pop*100, i, 'es' if i>1 else 'e'))
-print('The average human must learn {:.2f} phonemes to pronounce Djastiz words.'.format(avg_num_to_learn))
+print('The average human must learn {:.2f}/{} phonemes to pronounce Djastiz words.'.format(avg_num_to_learn, len(CASTISU)))
 
 print()
 
@@ -100,6 +112,6 @@ print()
 for phoneme in sorted(phonemes.keys(), key=lambda p:-phonemes[p]):
 	print("{}\t{:.2f}%".format(phoneme, 100*phonemes[phoneme]/total_pop))
 
-plt.pie(num_who_must_learn, labels=["Native"]+["{} phn.".format(i) for i in range(1,len(DJASTIZ)+1)], startangle=-90)
+plt.pie(num_who_must_learn, labels=["Native"]+["{} phn.".format(i) for i in range(1,len(CASTISU)+1)], startangle=-90)
 plt.axis('equal')
 plt.show()
